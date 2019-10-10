ba53bd4d4a110a5f07a8f7e2b764b969f817b432
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-10 04:16:30 -0300

    hero fullPage e ajusta no responsivo vagas

--- src/views/ListaVagas.vue
+++ src/views/ListaVagas.vue
@@ -1,5 +1,5 @@
 <template class="is-clipped">
-  <div class="container">
+  <div>
     <section class="hero is-info is-small">
       <div class="hero-body">
         <div class="container has-text-centered">
@@ -12,46 +12,48 @@
         </div>
       </div>
     </section>
-    <div class="section">
-      <div class="row columns is-multiline">
-        <div class="column is-one-third" v-if="!loading">
-          <b-field label="Repositório" label-position="on-border">
-            <b-select expanded v-model="filtroRepositorio">
-              <option
-                v-for="repositorio in $store.state.repositorios"
-                :value="repositorio._id"
-                :key="repositorio._id">
-                {{ repositorio._id }} ({{repositorio.count}})
-              </option>
-            </b-select>
-          </b-field>
+    <div class="container">
+      <div class="section">
+        <div class="row columns is-multiline">
+          <div class="column is-one-third" v-if="!loading">
+            <b-field label="Repositório" label-position="on-border">
+              <b-select expanded v-model="filtroRepositorio">
+                <option
+                  v-for="repositorio in $store.state.repositorios"
+                  :value="repositorio._id"
+                  :key="repositorio._id">
+                  {{ repositorio._id }} ({{repositorio.count}})
+                </option>
+              </b-select>
+            </b-field>
+          </div>
+          <div class="column is-half" v-if="!loading">
+            <b-field label="Filtro (avançado)" label-position="on-border">
+              <b-input placeholder="Ex.: javascript remot"
+                expanded
+                type="search"
+                v-model="filtroTituloVaga"
+                expanded
+                @keydown.native.enter="filtrarVagas(filtroTituloVaga, filtroRepositorio, tipoFiltroSelecionado)">
+              </b-input>
+            </b-field>
+          </div>
+          <div class="column is-two-third" v-if="!loading">
+            <p class="control">
+              <button class="button is-primary is-fullwidth" @click="filtrarVagas(filtroTituloVaga, filtroRepositorio, tipoFiltroSelecionado)">Filtrar</button>
+            </p>
+          </div>
+          <div class="column is-full-mobile is-half-tablet is-one-third-desktop is-one-quarter-fullhd" v-for="vaga in $store.state.vagas">
+            <router-link :to="`/vaga/${vaga._id}`">
+              <card-vaga :vaga="vaga"></card-vaga>
+            </router-link>
+          </div>
         </div>
-        <div class="column is-half" v-if="!loading">
-          <b-field label="Filtro (avançado)" label-position="on-border">
-            <b-input placeholder="Ex.: javascript remot"
-              expanded
-              type="search"
-              v-model="filtroTituloVaga"
-              expanded
-              @keydown.native.enter="filtrarVagas(filtroTituloVaga, filtroRepositorio, tipoFiltroSelecionado)">
-            </b-input>
-          </b-field>
+        <div class="centered-content" v-if="!loading && $store.state.vagas.length > 0">
+          <b-button size="is-large" type="is-success" outlined rounded @click="carregarVagas($store.state.paginaAtual + 1)">Buscar mais vagas</b-button>
         </div>
-        <div class="column is-two-third" v-if="!loading">
-          <p class="control">
-            <button class="button is-primary is-fullwidth" @click="filtrarVagas(filtroTituloVaga, filtroRepositorio, tipoFiltroSelecionado)">Filtrar</button>
-          </p>
-        </div>
-        <div class="column is-one-third" v-for="vaga in $store.state.vagas">
-          <router-link :to="`/vaga/${vaga._id}`">
-            <card-vaga :vaga="vaga"></card-vaga>
-          </router-link>
-        </div>
+        <b-loading :is-full-page="true" :active.sync="loading" :can-cancel="false"></b-loading>
       </div>
-      <div class="centered-content" v-if="!loading && $store.state.vagas.length > 0">
-        <b-button rounded @click="carregarVagas($store.state.paginaAtual + 1)">Buscar mais vagas</b-button>
-      </div>
-      <b-loading :is-full-page="true" :active.sync="loading" :can-cancel="false"></b-loading>
     </div>
   </div>
 </template>
@@ -92,14 +94,14 @@
 
       if (this.$store.state.vagas.length > 0)
         return;
-      
+
       this.carregarVagas();
     },
     methods: {
       carregarVagas(pagina = 1){
         this.$store.dispatch('buscarVagasNovaPagina', pagina);
       },
-      filtrarVagas(filtroTituloVaga, filtroRepositorio, tipoFiltro){ 
+      filtrarVagas(filtroTituloVaga, filtroRepositorio, tipoFiltro){
         this.$store.dispatch('buscarVagasFiltrando', {filtroTituloVaga: filtroTituloVaga, filtroRepositorio: filtroRepositorio, tipoFiltroSelecionado: tipoFiltro});
       }
     }
@@ -115,7 +117,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
-  }  
+  }
   .is-shady {
     animation: flyintoright .4s backwards;
     background: rgba(235, 235, 235, 0.705);
@@ -130,5 +132,5 @@
   .is-shady:hover {
     box-shadow: 0 10px 16px rgba(0, 0, 0, .13), 0 6px 6px rgba(0, 0, 0, .19);
   }
-  
+
 </style>
