335258bc5276c65bb79f519fe918194da90ca724
Ermesom Lourenço de Oliveira Barros <ermesoml.ufg@gmail.com>
2019-10-10 08:27:59 -0300

    Adicionando contribuinte @lucasferreiralimax
    Adicionado @lucasferreiralimax como contribuinte na página de Sobre.

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -10,24 +10,22 @@
         <div class="column is-full">
           <h1 class="title">Sobre o projeto</h1>
           <p>Este é um projeto opensource e possui o objetivo de reunir as vagas que estejam reunidas em diversos repositórios do github. Caso deseje que uma vaga apareça aqui basta publicar em um dos seguintes grupos:</p>
-          <li class="m-t-sm" v-for="repositorio in repositorios"><a :href="repositorio.url">{{repositorio.nome}}</a></li>
+          <li class="m-t-xs" v-for="repositorio in repositorios"><a :href="repositorio.url">{{repositorio.nome}}</a></li>
         </div>
         <div class="column is-full">
           <h1  class="title">Contribuintes</h1>
-          <a href="https://github.com/Ermesoml">
-            <div class="card large round is-shady">
+          <a :href="contribuinte.link_github" v-for="contribuinte in contribuintes">
+            <div class="card large round is-shady m-t-sm">
               <div class="card-content">
                 <div class="media">
                   <div class="media-left">
                     <figure class="image is-96x96">
-                      <img src="https://avatars0.githubusercontent.com/u/8354256?s=460&v=4" alt="Avagar URL" class="is-rounded">
+                      <img :src="contribuinte.avatar" alt="Avagar URL" class="is-rounded">
                     </figure>
                   </div>
                   <div class="content">
-                    <div class="title is-6 no-padding m-b-sm">Ermesom Lourenço</div>
-                    <a href="https://github.com/Ermesoml">https://github.com/Ermesoml</a>
-                    <br>
-                    <a href="https://www.linkedin.com/in/ermesomlourenco/">https://www.linkedin.com/in/ermesomlourenco/</a>
+                    <div class="title is-6 no-padding m-b-sm">{{contribuinte.nome}}</div>
+                    <a v-for="link in contribuinte.links" :href="link" style="display: block; margin-top: 2px">{{link}}</a>
                   </div>
                 </div>
               </div>
@@ -51,6 +49,26 @@ export default {
   name: 'Sobre',
   data(){
     return {
+      contribuintes:[
+        {
+          nome: 'Ermesom Lourenco',
+          avatar: 'https://avatars0.githubusercontent.com/u/8354256?s=460&v=4',
+          link_github: 'https://github.com/Ermesoml',
+          links: [
+            'https://github.com/Ermesoml',
+            'https://www.linkedin.com/in/ermesomlourenco/'
+          ]
+        },
+        {
+          nome: 'Lucas Ferreira',
+          avatar: 'https://avatars2.githubusercontent.com/u/40927839?s=460&v=4',
+          link_github: 'https://github.com/lucasferreiralimax',
+          links: [
+            'https://github.com/lucasferreiralimax',
+            'https://www.linkedin.com/in/lucasferreiralimax'
+          ]
+        }        
+      ],
       repositorios: [
         {
           nome: 'frontendbr',
